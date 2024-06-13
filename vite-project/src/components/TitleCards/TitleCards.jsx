35b3b0cedace7ced064710962ff6f27ae69c38df
Sarthak Gohel <sarthakgohel5555@gmail.com>
2024-06-13 08:55:59 -0400

    update

--- vite-project/src/components/TitleCards/TitleCards.jsx
+++ vite-project/src/components/TitleCards/TitleCards.jsx
@@ -3,7 +3,7 @@ import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 import { Link } from 'react-router-dom'
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
 
@@ -14,37 +14,39 @@ const TitleCards = ({title, category}) => {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjkxMGM1NTFiZjBkZDhhNjQyNTkyN2JhZmM1NmJjNCIsInN1YiI6IjY2Njg2MTA5NTViOTAxOTVmMWMyYWNmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8w7i7ueH3gxKg4ajiKmMNC64DGc34E8ubHmnnGZiE7o'
     }
   };
-  
-  
-  const handlewheel = (event)=>{
-    event.preventDefault;
-    cardsRef.current.scrollLeft += event.deltaY;
+
+  const handleWheel = (event) => {
+    event.preventDefault(); // Prevent default scrolling behavior
+    cardsRef.current.scrollLeft += event.deltaY; // Scroll horizontally
   }
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
-    .then(response => response.json())
-    .then(response => setApiData(response.results))
-    .catch(err => console.error(err));
-    const currentRef = cardsRef.current;
-    currentRef.addEventListener('wheel', handlewheel);
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => setApiData(response.results))
+      .catch(err => console.error(err));
 
-    
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel, { passive: false }); // Add event listener with passive: false
+
     return () => {
-      currentRef.removeEventListener('wheel', handlewheel);
+      currentRef.removeEventListener('wheel', handleWheel); // Remove event listener on component unmount
     };
   }, []);
+
   return (
-    <div className='titlecards' >
-        <h2>{title?title:"Popular on Netflix"}</h2>
-        <div className="card-list" ref={cardsRef}>
-          {apiData.map((card,index)=> {
-            return <Link to={`/player/${card.id}`} className="card" key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+    <div className='titlecards'>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
               <p>{card.original_title}</p>
             </Link>
-          })}
-        </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
